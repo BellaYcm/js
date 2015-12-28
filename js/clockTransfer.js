f269a47e2883f5ec9666eaab977ec34ac12ef97a
sunke <sunkejl@live.com>
2015-12-28 15:49:03 +0800

    transfer

---
+++ js/clockTransfer.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * Created by sunke on 2015/12/28.
+ */
+window.onload=
+    function(){
+        var clock=document.getElementById("clock"),
+            icon=new Image();
+        icon.src="clock-icon.png";
+        function displayTime(){
+            var now =new Date(),
+                hrs=now.getHours(),
+                mins=now.getMinutes();
+            if (mins<10)
+            {
+                mins="0"+mins;
+            }
+            clock.innerHTML=hrs+":"+mins;
+            setTimeout(displayTime,60000);
+        }
+        displayTime();
+        clock.draggable=true;
+        clock.ondragstart=function(event){
+            var event=event||window.event;
+            var dt=event.dataTransfer;
+            dt.setData("Text",Date()+"\n");
+            if(dt.setDragImage){
+                dt.setDragImage(icon,0,0);
+            }
+        }
+    }
+;
